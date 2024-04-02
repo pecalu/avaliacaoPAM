56add0cc5c98c1a8e1c45713c45fe28393803824
pecalu <124682526+pecalu@users.noreply.github.com>
2024-04-02 12:41:16 -0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace avaliacaoPAM
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void rolarDados(object sender, EventArgs e)
+        {
+
+            ValorSorteado.Text = new Random().Next(1, 11).ToString();
+        }
+    }
+
+}
